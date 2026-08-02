b3aa030a956007efb1dc4a0ed3272be830f5e80d
pavankumar <144452510+pavankumarrcs22@users.noreply.github.com>
2026-08-02 10:47:05 +0530

    Initial Commit

---
+++ src/app/home/home.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Navbar } from '../navbar/navbar';
+
+@Component({
+  selector: 'app-home',
+  imports: [Navbar],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {}
